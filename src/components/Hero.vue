bdf374c35485e2950a42512d8ccece665e578928
harisanawar129 <harisanawar129@gmail.com>
2022-06-28 17:19:16 +0700

    tahap ke 2

---
+++ src/components/Hero.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="row mt-2">
+<div class="col">
+   <div class="d-flex h-100">
+   <div class="justyfy-content-center align-self-center">
+   <h2><strong>Menu Makan Enak,</strong>Bisa Dipesan Sekarang</h2>
+   <button class="btn btn-lg btn-success"><b-icon-arrow-right></b-icon-arrow-right> Pesan </button>
+   </div>
+   </div>
+</div>
+<div class="col">
+<img src="../assets/images/hero.png" width="100%">
+</div>
+</div>
+</template>
+
+<script>
+
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Navbar'
+ 
+}
+</script>
+
+<style >
+    
+</style>
---
+++ src/views/Foods.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <Navbar/>
+    <h2>Daftar Makanans</h2>
+</div>
+</template>
+
+<script>
+
+import Navbar from '@/components/Navbar.vue'
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Foods',
+  components:{
+      Navbar
+  }
+  
+}
+</script>
