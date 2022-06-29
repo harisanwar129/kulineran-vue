fce34496d456fd554557449cb9c237fd609b72c7
harisanawar129 <harisanawar129@gmail.com>
2022-06-29 12:27:24 +0700

    eror

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,18 @@
 <template>
-<div class="row mt-2">
+<div class="row mt-4">
+<div class="col">
+   <div class="d-flex h-100">
+   <div class="justyfy-content-center align-self-center">
+   <h2><strong>Menu Makan Enak,</strong>Bisa Dipesan Sekarang</h2>
+   <button class="btn btn-lg btn-success"><b-icon-arrow-right></b-icon-arrow-right> Pesan </button>
+   </div>
+   </div>
+</div>
+<div class="col">
+<img src="../assets/images/hero.png" width="100%">
+</div>
+</div>
+<div class="row mt-4">
 <div class="col">
    <div class="d-flex h-100">
    <div class="justyfy-content-center align-self-center">
